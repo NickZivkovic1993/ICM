b7bda41880e19386dc888b48dbdc60469b5dc6e7
git@github.com:NickZivkovic1993/RareCrewSolution.git <git@github.com:NickZivkovic1993/RareCrewSolution.git>
2024-02-16 14:46:05 +0100

    Added WPF dependency injections

---
+++ ICMWPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICMWPFUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
